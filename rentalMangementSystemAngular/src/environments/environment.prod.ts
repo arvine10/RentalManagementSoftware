fb46de5abc3329a6707d2e1c40c20a7daffff3d2
Arvine Rastegar <arvinerastegar@gmail.com>
2023-08-07 12:14:12 -0500

    commit code

---
+++ rentalMangementSystemAngular/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  cognito: {
+    userPoolId: 'fillInHere',
+    userPoolWebClientId: 'EnterCodeHeree',
+  },
+  baseURL:
+    'http://960176rentalmanagement-specficElasticBeanStalkInstance',
+};
